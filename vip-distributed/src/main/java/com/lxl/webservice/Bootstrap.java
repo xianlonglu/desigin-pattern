57f0e16b67a8508c5981c6133e0dfb3c9eb3120c
rookies <408030514@qq.com>
2019-04-15 15:56:13 +0800

    add 2018serialize

--- vip-distributed/src/main/java/com/lxl/webservice/Bootstrap.java
+++ vip-distributed/src/main/java/com/lxl/webservice/Bootstrap.java
@@ -11,7 +11,7 @@ public class Bootstrap {
 
     public static void main(String[] args) {
 
-        Endpoint.publish("http://localhost:8080/vip/hello",new SayHelloImpl());
+        Endpoint.publish("http://localhost:8888/vip/hello",new SayHelloImpl());
 
         System.out.println("publish success");
     }
