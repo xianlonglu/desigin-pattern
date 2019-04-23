6830c571deae5112bfeca292b811a0a856b9aefd
rookies <408030514@qq.com>
2019-04-23 20:07:27 +0800

    update vip-order-api 发布增加配置，客户端通过import直接调用

--- vip-dubbo/vip-dubbo-order/vip-order-provider/src/main/java/com/gupao/vip/mic/dubbo/order/ProviderApp.java
+++ vip-dubbo/vip-dubbo-order/vip-order-provider/src/main/java/com/gupao/vip/mic/dubbo/order/ProviderApp.java
@@ -1,14 +1,28 @@
 package com.gupao.vip.mic.dubbo.order;
 
+import java.io.IOException;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.alibaba.dubbo.container.Main;
 
 /**
  * Hello world!
  *
  */
-public class ProviderApp 
-{
-    public static void main( String[] args ) {
-        Main.main(args);
-    }
+public class ProviderApp {
+	public static void main(String[] args) throws IOException {
+		// 启动方式1
+		// ClassPathXmlApplicationContext context = new
+		// ClassPathXmlApplicationContext(
+		// "META-INF/spring/order-provider.xml.xml");
+		// context.start();
+		// System.in.read(); // 阻塞当前进程
+
+		// 启动方式2, 启动多个容器
+		// Main.main(new String[]{"Spring", "log4j"});
+
+		// 启动方式3
+		Main.main(args);// Main.main(new String[]{"Spring"});// 等价
+	}
 }
